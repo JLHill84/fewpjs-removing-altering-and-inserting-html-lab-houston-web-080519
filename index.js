b55e452f6aae4495f92e40b2adb937fd137305fc
Joshua Hill <josh@jlhworkshop.com>
2019-09-16 16:55:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 document.querySelector('main#main').remove();
 let newHeader = document.createElement('h1');
 newHeader.id = "victory";
-newHeader.innerText = "YOUR NAME is the champion";
+newHeader.innerHTML = "YOUR NAME is the champion";
